7f81710f618536855c0946747067f6b6650981ca
Misha-Sh <msh2isr@gmail.com>
2018-08-09 23:59:55 +0300

    Исправление 11 и 12 ДЗ
    11 ДЗ - сделан генератор уникального емейла, укорочены локаторы.
    12 ДЗ - укорочены локаторы, картинка продукта перенесена в папку resources

--- home-works/src/test/java/hw11_FillForm/Registration.java
+++ home-works/src/test/java/hw11_FillForm/Registration.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class Registration {
@@ -31,12 +33,12 @@ public class Registration {
 //        options.setBinary(new FirefoxBinary(new File("/Applications/FireFox45ESR/Firefox.app/Contents/MacOS/firefox-bin")));
 //        driver = new FirefoxDriver(options);
 //        ///////////for FireFox Common ///////////////////////
-        FirefoxOptions options = new FirefoxOptions();
-          options.setBinary(new FirefoxBinary(new File("/Applications/Firefox.app/Contents/MacOS/firefox-bin")));
-          driver = new FirefoxDriver(options);
+//        FirefoxOptions options = new FirefoxOptions();
+//          options.setBinary(new FirefoxBinary(new File("/Applications/Firefox.app/Contents/MacOS/firefox-bin")));
+//          driver = new FirefoxDriver(options);
 //////////////////////////////////////////////////////////////////
         ////////////// For Chrome ///////////////////
-        //driver = new ChromeDriver();
+        driver = new ChromeDriver();
         //////////////////////For Safari ////////////////////
         //driver = new SafariDriver();
 /////////////////////////////////////////////////////////////
@@ -46,35 +48,39 @@ public class Registration {
     }
     @Test
     public void test(){
+        UUID uniqueKey = UUID.randomUUID();
+        String pref = uniqueKey.toString();
+        String email = "name"+pref + "@pip.ru";
+
         driver.get("http://localhost/litecart/en/create_account");
       //WebElement form = driver.findElement(By.cssSelector("div#create-account div form input"));
       // ((JavascriptExecutor) driver).executeScript("arguments[0].style.opacity=1;", form);
-     driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='tax_id']")).sendKeys(Keys.HOME + "TaxID030303");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='company']")).sendKeys(Keys.HOME + "Meteor");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='firstname']")).sendKeys(Keys.HOME + "FirstName");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='lastname']")).sendKeys(Keys.HOME + "LastName");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='address1']")).sendKeys(Keys.HOME + "Address1");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='address2']")).sendKeys(Keys.HOME + "Address2");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='postcode']")).sendKeys(Keys.HOME + "54321");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='city']")).sendKeys(Keys.HOME + "Lubertsy");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='email']")).sendKeys(Keys.HOME + "pet24@pet.pet");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='phone']")).sendKeys(Keys.HOME + "+7 232 333 5544");
+     driver.findElement(By.cssSelector("div#create-account  input[name='tax_id']")).sendKeys(Keys.HOME + "TaxID030303");
+        driver.findElement(By.cssSelector("div#create-account input[name='company']")).sendKeys(Keys.HOME + "Meteor");
+        driver.findElement(By.cssSelector("div#create-account input[name='firstname']")).sendKeys(Keys.HOME + "FirstName");
+        driver.findElement(By.cssSelector("div#create-account input[name='lastname']")).sendKeys(Keys.HOME + "LastName");
+        driver.findElement(By.cssSelector("div#create-account input[name='address1']")).sendKeys(Keys.HOME + "Address1");
+        driver.findElement(By.cssSelector("div#create-account input[name='address2']")).sendKeys(Keys.HOME + "Address2");
+        driver.findElement(By.cssSelector("div#create-account input[name='postcode']")).sendKeys(Keys.HOME + "54321");
+        driver.findElement(By.cssSelector("div#create-account input[name='city']")).sendKeys(Keys.HOME + "Lubertsy");
+        driver.findElement(By.cssSelector("div#create-account input[name='email']")).sendKeys(Keys.HOME + email);
+        driver.findElement(By.cssSelector("div#create-account input[name='phone']")).sendKeys(Keys.HOME + "+7 232 333 5544");
 
-        WebElement countries = driver.findElement(By.cssSelector("div#create-account div form table tbody tr td select[name='country_code']"));
+        WebElement countries = driver.findElement(By.cssSelector("div#create-account select[name='country_code']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].selectedIndex=224; arguments[0].dispatchEvent(new Event ('change'));", countries);
 
-        WebElement zones = driver.findElement(By.cssSelector("div#create-account div form table tbody tr td select[name='zone_code']"));
+        WebElement zones = driver.findElement(By.cssSelector("div#create-account select[name='zone_code']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].selectedIndex=5; arguments[0].dispatchEvent(new Event ('change'));", zones);
 
 
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td label input[name='newsletter']")).click();
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='password']")).sendKeys(Keys.HOME + "mypassword");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td input[name='confirmed_password']")).sendKeys(Keys.HOME + "mypassword");
-        driver.findElement(By.cssSelector("div#create-account div form table tbody tr td button[name='create_account']")).click();
+        driver.findElement(By.cssSelector("div#create-account input[name='newsletter']")).click();
+        driver.findElement(By.cssSelector("div#create-account input[name='password']")).sendKeys(Keys.HOME + "mypassword");
+        driver.findElement(By.cssSelector("div#create-account input[name='confirmed_password']")).sendKeys(Keys.HOME + "mypassword");
+        driver.findElement(By.cssSelector("div#create-account button[name='create_account']")).click();
 
         driver.findElement(By.linkText("Logout")).click();
 
-        driver.findElement(By.cssSelector("#box-account-login input[name='email']")).sendKeys(Keys.HOME + "pet24@pet.pet");
+        driver.findElement(By.cssSelector("#box-account-login input[name='email']")).sendKeys(Keys.HOME + email);
         driver.findElement(By.cssSelector("#box-account-login input[name='password']")).sendKeys(Keys.HOME + "mypassword");
         driver.findElement(By.cssSelector("#box-account-login button[name ='login']")).click();
 
--- home-works/src/test/java/hw12_NewGood/NewGood.java
+++ home-works/src/test/java/hw12_NewGood/NewGood.java
@@ -58,73 +58,71 @@ public class NewGood {
         driver.findElement(By.cssSelector("ul#box-apps-menu li:nth-child(2)")).click();
         driver.findElement(By.cssSelector("td#content div a:nth-child(2)")).click();
 
-        driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(1) > td > label:nth-child(3) > input[type='radio']")).click();
-        driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(2) td span input")).sendKeys("Duckling1");
-        driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(3) td input")).sendKeys("D0001");
-        driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(4) td div table tbody tr:nth-child(2) td:nth-child(1) input[type='checkbox']")).click();
+        //driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(1) > td > label:nth-child(3) > input[type='radio']")).click();
+        driver.findElement(By.cssSelector("#tab-general input[value = '1']")).click();
+
+        driver.findElement(By.cssSelector("#tab-general input[name = 'name[en]']")).sendKeys("Duckling1");
+        driver.findElement(By.cssSelector("#tab-general input[name = 'code']")).sendKeys("D0001");
+        driver.findElement(By.cssSelector("#tab-general input[data-name = 'Rubber Ducks']")).click();
 
 
 
-        WebElement def_cat = driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(5) td select"));
+        WebElement def_cat = driver.findElement(By.cssSelector("#tab-general select[name = 'default_category_id']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].selectedIndex=1; arguments[0].dispatchEvent(new Event ('change'));", def_cat);
 
-        driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(7) td div table tbody tr:nth-child(4) td:nth-child(1) input[type='checkbox']")).click();
-        WebElement number = driver.findElement(By.cssSelector("#tab-general table tbody tr:nth-child(8) td table tbody tr td:nth-child(1) input[type='number']"));
+        driver.findElement(By.cssSelector("#tab-general input[value='1-3']")).click();
+        WebElement number = driver.findElement(By.cssSelector("#tab-general input[name='quantity']"));
         number.clear();
         number.sendKeys("200");
 
-    WebElement fileInput = driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(9) > td > table > tbody > tr:nth-child(1) > td > input[type='file']"));
+    WebElement fileInput = driver.findElement(By.cssSelector("#tab-general input[type='file']"));
 
-        Path file = Paths.get("/Users/int58/Documents/GitHub/SelWebDriveFull-1/home-works/src/test/java/hw12_NewGood/veloSea.jpg");
+        Path file = Paths.get("/Users/int58/Documents/GitHub/SelWebDriveFull-1/home-works/src/test/resources/veloSea.jpg");
         Path folder = Paths.get("/Users/int58/Documents/GitHub/SelWebDriveFull-1/home-works");
        Path relativePath = folder.relativize(file);
 String filePath = relativePath.toAbsolutePath().toString();
 fileInput.sendKeys(filePath);
 
 
-driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(10) > td > input[type='date']")).sendKeys("08.08.2018");
-driver.findElement(By.cssSelector("#tab-general > table > tbody > tr:nth-child(11) > td > input[type='date']")).sendKeys("08.08.2020");
+driver.findElement(By.cssSelector("#tab-general input[name='date_valid_from']")).sendKeys("08.08.2018");
+driver.findElement(By.cssSelector("#tab-general input[name='date_valid_to']")).sendKeys("08.08.2020");
 
 
-        driver.findElement(By.cssSelector("#content > form > div > ul > li:nth-child(2) > a")).click();
+        driver.findElement(By.cssSelector("#content a[href = '#tab-information']")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebElement manufacturer = driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(1) > td > select"));
+        WebElement manufacturer = driver.findElement(By.cssSelector("#tab-information select[name = 'manufacturer_id']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].selectedIndex=1; arguments[0].dispatchEvent(new Event ('change'));", manufacturer);
 
 
-driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(3) > td > input[type='text']")).sendKeys("duck, duckling, water");
-driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(4) > td > span > " +
-        "input[type='text']")).sendKeys("New little duck");
-driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(5) > td" +
-        " > span > div > div.trumbowyg-editor")).sendKeys("A small unsinkable duckling will be with your children in all waters");
-    driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(6) > td > " +
-            "span > input[type='text']")).sendKeys("Duckling");
+driver.findElement(By.cssSelector("#tab-information input[name ='keywords']")).sendKeys("duck, duckling, water");
+driver.findElement(By.cssSelector("#tab-information input[name ='short_description[en]']")).sendKeys("New little duck");
+driver.findElement(By.cssSelector("#tab-information div.trumbowyg-editor")).sendKeys("A small unsinkable duckling will be with your children in all waters");
+    driver.findElement(By.cssSelector("#tab-information input[name ='head_title[en]']")).sendKeys("Duckling");
 
-        driver.findElement(By.cssSelector("#tab-information > table > tbody > tr:nth-child(7) > td > span > input[type='text']")).sendKeys("Meta Duckling");
+        driver.findElement(By.cssSelector("#tab-information input[name='meta_description[en]']")).sendKeys("Meta Duckling");
 
-        driver.findElement(By.cssSelector("#content > form > div > ul > li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("#content a[href = '#tab-prices']")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
-WebElement pricePurchase = driver.findElement(By.cssSelector("#tab-prices > table:nth-child(2) > tbody > tr > td > input[type=\"number\"]"));
+WebElement pricePurchase = driver.findElement(By.cssSelector("#tab-prices input[name = 'purchase_price']"));
         pricePurchase.clear();
         pricePurchase.sendKeys("1");
 
-        WebElement pricePurchaseValuta = driver.findElement(By.cssSelector("#tab-prices > table:nth-child(2) > tbody > tr > td > select"));
+        WebElement pricePurchaseValuta = driver.findElement(By.cssSelector("#table-prices select[name = 'tax_class_id']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].selectedIndex=1; arguments[0].dispatchEvent(new Event ('change'));", pricePurchaseValuta);
 
-WebElement priceUSD = driver.findElement(By.cssSelector("#tab-prices > table:nth-child(4) > tbody > tr:nth-child(2) > td:nth-child(1) > span > input[type='text']"));
+WebElement priceUSD = driver.findElement(By.cssSelector("#tab-prices input[name='prices[USD]']"));
     priceUSD.clear();
     priceUSD.sendKeys("1.5");
 
-    WebElement priceEVRO = driver.findElement(By.cssSelector("#tab-prices > table:nth-child(4) > tbody >" +
-            " tr:nth-child(3) > td:nth-child(1) > span > input[type='text']"));
+    WebElement priceEVRO = driver.findElement(By.cssSelector("#tab-prices input[name='prices[EUR]']"));
 
     priceEVRO.clear();
     priceEVRO.sendKeys("0.7");
 
-    driver.findElement(By.cssSelector("#content > form > p > span > button:nth-child(1)")).click();
+    driver.findElement(By.cssSelector("#content button[name = 'save']")).click();
 
     driver.get("http://localhost/litecart/en/");
     driver.findElement(By.cssSelector("a[title = 'Duckling1']")).click();
